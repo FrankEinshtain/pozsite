7a4b33dfbf872821934784b486809d2d5aa552fc
frankie <r.rudchenko@nton.info>
2020-06-24 12:59:17 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown`,
-        path: `${__dirname}/src/markdown`,
+        path: path.join(__dirname, `src`, `markdown`),
       },
     },
     {
